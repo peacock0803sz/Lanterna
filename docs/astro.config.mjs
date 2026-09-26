4216f2bbebabbcf97a658e776623cf652e4116e0
Peacock <33555487+peacock0803sz@users.noreply.github.com>
2026-09-26 13:19:05 +0900

    :art: Add site styles for docs

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 export default defineConfig({
+  css: ['./src/styles/custom.css'],
   integrations: [
     starlight({
       title: 'Lanterna',
